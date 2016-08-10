98300c690ed72552962d1c2b84560d584b8f5057
KyrieSu <watermelo0326@gmail.com>
2016-08-10 15:27:41 +0800

    parse querystring via url

--- example.js
+++ example.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var url = require('url')
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type' : 'text/html'});
     res.write('Hello World ~');
+    var query = url.parse(req.url,true).query;
+    console.log(query);
     res.end();
 }).listen(1234);
 
