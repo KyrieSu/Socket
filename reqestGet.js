54b07dbab454c10400b222677504a54a33ed7da2
KyrieSu <watermelo0326@gmail.com>
2016-08-13 01:10:59 +0800

    get methon v2.0

--- reqestGet.js
+++ reqestGet.js
@@ -1,9 +1,26 @@
 var request = require('request');
 
- request('http://127.0.0.1:1234?name=kyrie',function(err,res,body){
+/* request('http://127.0.0.1:1234?name=kyrie',function(err,res,body){
      if(err){
          console.log(err);
          return;
      }
      console.log(res.statusCode);
- })
+}) */
+
+var option = {
+    url : 'http://127.0.0.1:1234',
+    qs  : {
+        name : 'KyrieSu',
+        sex  : 'male'
+    }
+}
+
+
+request.get(option,function(err,res,body){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(res.statusCode);
+})
